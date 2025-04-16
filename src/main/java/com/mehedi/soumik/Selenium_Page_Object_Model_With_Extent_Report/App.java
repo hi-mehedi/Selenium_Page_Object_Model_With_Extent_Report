de2f541d1ad9d57dc56262b9a2dc4652e95a0c29
mehedi-3311 <mehedi15-3311@diu.edu.bd>
2025-04-16 16:38:11 +0600

    Version 1.00

---
+++ src/main/java/com/mehedi/soumik/Selenium_Page_Object_Model_With_Extent_Report/App.java
@@ -0,0 +1,13 @@
+package com.mehedi.soumik.Selenium_Page_Object_Model_With_Extent_Report;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/mehedi/hasan/drivers/BaseDriver.java
@@ -0,0 +1,41 @@
+package com.mehedi.hasan.drivers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseDriver {
+
+	protected WebDriver driver;
+	@BeforeSuite
+	public void StartBrowser() {
+		String webBrowser = "chrome";
+		if(webBrowser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver= new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		else if(webBrowser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			driver.manage().window().maximize();
+		}
+		else {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+		}
+		
+		PageDriver.getInstance().setDriver(driver);
+
+	}
+	@AfterSuite
+	public void closeBrowse() {
+        driver.close();
+  }
+}
